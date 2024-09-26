d749097a03e29381649995c1711bf7f14f5a10f3
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-09-26 20:05:31 +0300

    basename

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/events-registration-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
